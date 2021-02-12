341fb00f3a73061034f9cb6f218ceaf2efe594a7
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-11 23:44:15 -0800

    Setup Elasticsearch environment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # oomck-bot
+
+## Getting started
+
+### Dependancies
+
+- [Docker] (https://www.docker.com/get-started)
+- [Python 3] (https://www.python.org/downloads/)
+
+### Setup Environment
+
+1. Start ElasticSearch container `docker run -d --name elasticsearch -p 9300:9300 -p 9200:9200 elasticsearch:7.6.2`
+
+2. `python init.py`
---
+++ init.py
@@ -0,0 +1,25 @@
+from elasticsearch import Elasticsearch
+
+# Connect to Elastic search
+esclient = Elasticsearch(["localhost:9200"])
+
+# EXAMPLE QUERY
+#
+# response = esclient.search(
+#     index='social-*',
+#     body={
+#         "query": {
+#             "match": {
+#                 "message": "myProduct"
+#             }
+#         },
+#         "aggs": {
+#             "top_10_states": {
+#                 "terms": {
+#                     "field": "state",
+#                     "size": 10
+#                 }
+#             }
+#         }
+#     }
+# )

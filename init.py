5123650a70657a1ca297de6952ddf38e9b4ece4a
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-22 10:09:24 -0800

    automate data download OOMCKBOT-27

--- init.py
+++ init.py
@@ -1,5 +1,14 @@
 from elasticsearch import Elasticsearch
 from bot import Bot
+import sys
+import os
+
+# Download data
+data_repo = "https://github.com/google-research-datasets/ccpe"
+
+if not os.path.isdir("./ccpe"):
+    os.system("git clone " + data_repo)
+
 
 # Connect to Elastic search
 esclient = Elasticsearch(["localhost:9200"])

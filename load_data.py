1d6b64fb27b71a15b3a06a3b5d1d48cf48b1c41d
Carson Ricca <carsonricca28@gmail.com>
2021-02-23 19:49:22 -0800

    OOMCKBOT-29: Data now loads successfully

--- load_data.py
+++ load_data.py
@@ -3,33 +3,37 @@
 import json
 
 from elasticsearch import Elasticsearch, helpers
+from elasticsearch.helpers import BulkIndexError
 
-from search_data import search_by_index_and_id
 
 es = Elasticsearch()
+
+# Constants
+data = "data/data.json"
+index = "my-index"
 
 
 # Method to load the data from JSON file.
 def load_json_data():
-    with open("test_data.json") as file:
+    with open(data, 'r') as file:
         return json.load(file)
 
 
 # Method to insert the data from the JSON file into ElasticSearch.
-def insert_data_by_bulk(data):
-    response = helpers.bulk(es, data)
-    print(response)
+def insert_data_by_bulk(_data):
+    try:
+        helpers.bulk(es, _data, index=index)
+    except BulkIndexError:
+        print("Please increase your docker partition. This error occurs when there is not enough space to upload the "
+              "rest of the data to the Docker ElasticSearch container.")
 
 
 # Method to delete all the data from ElasticSearch.
 def delete_data():
-    es.indices.delete("test-index")
+    es.indices.delete(index)
 
 
 # Main method that utilizes the above methods to insert the data, and search for a specific instance in ElasticSearch.
 if __name__ == "__main__":
-    demo_data = load_json_data()
-    insert_data_by_bulk(demo_data)
-    res = search_by_index_and_id("test-index", 1)
-    print(res)
-
+    ubuntu_data = load_json_data()
+    insert_data_by_bulk(ubuntu_data)

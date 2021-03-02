ad219dfaed5f9623124632689b40f5484b5d333b
Mathew de Vin <mathewdevin03@gmail.com>
2021-03-02 11:07:46 -0800

    Retrieving the CLI input
    Modified the Bot class to include CLI input alongside storing the client elastic search inside of the class. This can then be used to call queries based on the user input.
    
    From: [OOMCKBOT-73]

--- bot.py
+++ bot.py
@@ -1,13 +1,20 @@
-import re
+from text_parser import Parser
 
 
 class Bot:
-    # Prompt user and split string into list of words
-    def prompt_user(self, prompt):
-        return input(prompt + "\n")
 
-    # Process string list input
-    def process_input(self, input):
-        # Split by spaces/symbols and remove empty strings
-        result = list(filter(None, re.split("[^A-Za-z]", input)))
-        return result
+    def __init__(self, elastic_client):
+        self.parser = Parser()
+        self.elastic = elastic_client
+        print("Welcome to the UBUNTU help bot! What can I help you with?")
+
+    def _elastic_search(self, query):
+        return ""  # self.elastic.search(index="test", body={"query": {"match_all": {}}})
+
+    def get_input(self):
+        user_input = input("[You]: ")
+        return self.parser.parse(user_input)
+
+    def get_answer(self, user_input):
+        response = self._elastic_search(user_input)
+        print("response")
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from text_parser import Parser
 from bot import Bot
 
 client = init.ensure_elastic()
-parser = Parser()
-
-print(parser.parse("Hello sir, how can I use for loop in bash script?"))
+bot = Bot(client)
+print(bot.get_input())
+bot.get_answer("open documents")

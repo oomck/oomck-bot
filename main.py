26e511c878003b74fdc5b5a5e66c69ef4e41ee7d
keyvankhademi <keyvankhademi@gmail.com>
2021-03-09 20:29:56 -0800

    Update readme

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from bot import Bot
+from es.data_loader import DataLoader
 
 bot = Bot()
+data_loader = DataLoader()
 
 while True:
     user_input = input("[YOU] ")
     if user_input == "exit":
         break
+    if user_input == "load_data":
+        data_loader.load_data()
+
     print("[BOT] " + bot.ask(user_input))
 

e56a625e2db7224cda140b9760cd107d0445e6c2
opeyem1a <opeyadeyemi@gmail.com>
2021-03-12 06:35:53 -0800

    auto load data once before the user is given the option to input text, also confirm through output when data has been loaded

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ bot = Bot()
 data_loader = DataLoader()
 
 print("Welcome to Oomck-Bot! Please type your message below:\nWaiting on you...")
+data_loader.load_data()
+
 while True:
     user_input = input("[YOU] ")
     if user_input == "exit":
         break
     if user_input == "load_data":
         data_loader.load_data()
-
-    print("[BOT] " + bot.ask(user_input))
+        print("Data has been loaded.")
+    else:
+        print("[BOT] " + bot.ask(user_input))
 

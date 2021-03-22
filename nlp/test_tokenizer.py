3804eb181482ac63ceb2062e67a7192bffbf836b
keyvankhademi <keyvankhademi@gmail.com>
2021-03-21 22:54:46 -0700

    Initialize nltk

--- nlp/test_tokenizer.py
+++ nlp/test_tokenizer.py
@@ -7,5 +7,5 @@ class TestTokenizer(unittest.TestCase):
     def test_tokenize(self):
         self.assertEqual(
             Tokenizer.tokenize("Hello, My name is Mike OXLONG!"),
-            ["Hello,", "My", "name", "is", "Mike", "OXLONG!"]
+            ["Hello", ",", "My", "name", "is", "Mike", "OXLONG", "!"]
         )

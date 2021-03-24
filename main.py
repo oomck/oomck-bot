cb48c20acb76635c67875f00ceaa314033ea128f
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-24 14:17:24 -0700

    Integrate with bot OOMCKBOT-117

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from bot import Bot
 from es.data_loader import DataLoader
+import eel
 
 bot = Bot()
 data_loader = DataLoader()
 
-print("Welcome to Oomck-Bot! Please type your message below:\nWaiting on you...")
 data_loader.load_data()
 
-while True:
-    user_input = input("[YOU] ")
-    if user_input == "exit":
-        break
-    if user_input == "load_data":
-        data_loader.load_data()
-        print("Data has been loaded.")
-    else:
-        print("[BOT] " + bot.ask(user_input))
+eel.init("web")
 
+@eel.expose
+def query_bot(user_input):
+    eel.send_bot_response(bot.ask(user_input))
+
+@eel.expose
+def load_data():
+    data_loader.load_data()
+
+eel.start("index.html")

4e33ade320770c10a91f563fe0ca3354446babf0
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-24 13:18:04 -0700

    Setup static gui OOMCKBOT-116

---
+++ gui.py
@@ -0,0 +1,8 @@
+import eel
+import random
+from datetime import datetime
+
+
+if __name__ == "__main__":
+    eel.init("web")
+    eel.start("index.html")
---
+++ web/js/main.js
@@ -0,0 +1,3 @@
+const loadButton = document.getElementById("load-data");
+const chatContainer = document.getElementById("chat");
+const sendButton = document.getElementById("send");

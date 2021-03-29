b2112d6f563c48aecc404fa26c080c4a8cd478ee
opeyem1a <opeyadeyemi@gmail.com>
2021-03-28 19:28:01 -0700

    added more default responses to OOMCKBOT's repertoire

--- bot/bot.py
+++ bot/bot.py
@@ -22,4 +22,17 @@ class Bot:
         if len(results) > 0:
             return results[0]["_source"]['response']
 
-        return "Sorry, I don't get what you're saying, can you try again?"
+        from random import randint
+        default = [
+            "Sorry, I don't get what you're saying, can you try again?",
+            "I don't quite follow, could you try repeating that in a different way?",
+            "I'm not sure if I remember that happening in the Fast & Furious movies...",
+            "Are you describing a specific scene? Which characters were involved?",
+            "You're interesting...Out of curiosity, what is your favourite Fast & Furious movie?",
+            "I don't understand, but I'm interested, what do you think Dom would say if you said that to him?",
+            "Well...I'm not sure how to respond. Let's change the topic - What do you think about Tyrese?",
+            "I don't know how to follow that up haha. To change the topic - What do you think about Dom?",
+            "Intriguing, but let's get back on topic - What do you think about Paul Walker?"
+        ]
+
+        return default[randint(0, len(default)-1)]

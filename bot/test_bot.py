75fed20843ba43eaaaf451df57cbaaac1a1e22c2
opeyem1a <opeyadeyemi@gmail.com>
2021-04-15 01:58:23 -0700

    updating test cases for the Bot class due to automatic translation

--- bot/test_bot.py
+++ bot/test_bot.py
@@ -10,4 +10,10 @@ class TestBot(unittest.TestCase):
 
     def test_bot(self):
         self.assertEqual(self.bot.ask("hi"), "Hey.")
-        self.assertIn(self.bot.ask("sadfkljdfkjlhfd"), DEFAULT)
+
+    def test_bot_unknown_string(self):
+        from nlp.translate import Translate
+        test = "sadfkljdfkjlhfd"
+        detected_lang = Translate.translate_to_english(test)[1]
+        translated_default = [Translate.translate_to_lang(i, detected_lang)[0] for i in DEFAULT]
+        self.assertIn(self.bot.ask(test), translated_default)
